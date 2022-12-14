0edd013d0c3078a798c3c268d14866a38bf86e02
vati <v.timova@it-frankfurt.com>
2022-12-14 11:32:35 +0100

    Initial Commit

---
+++ src/test/java/guru/springframework/ChuckNorris/ChuckNorrisApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.ChuckNorris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
